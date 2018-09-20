b264ea862cbca4b0852e7755279dc6a8ba5651a9
孙超 <2061857679@qq.com>
2018-09-20 18:03:07 +0800

    fdsaf

--- authentication-server1/src/main/java/com/scspringcloud/Oauth2authenticationserver1Application.java
+++ authentication-server1/src/main/java/com/scspringcloud/Oauth2authenticationserver1Application.java
@@ -3,12 +3,11 @@ package com.scspringcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.web.context.annotation.ApplicationScope;
 
 @SpringBootApplication
-@ComponentScan("com.scspringcloud.*")
-@EnableAuthorizationServer
 public class Oauth2authenticationserver1Application {
 
     public static void main(String[] args) {
--- authentication-server1/src/main/java/com/scspringcloud/config/Oauth2Config.java
+++ authentication-server1/src/main/java/com/scspringcloud/config/Oauth2Config.java
@@ -1,56 +1,32 @@
 package com.scspringcloud.config;
 
-import io.micrometer.core.instrument.util.TimeUtils;
-import org.omg.CORBA.TIMEOUT;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
-import java.util.concurrent.TimeUnit;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 /**
  * Created by Administrator on 2018/9/19.
  */
-@Configurable
+@Configuration
+@EnableAuthorizationServer
 public class Oauth2Config extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private DataSource dataSource;
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
-    }
-
-    @Bean
-    public ClientDetailsService clientDetails() {
-        return new JdbcClientDetailsService(dataSource);
-    }
 
 
+    /**
+     * 用来配置客户端详情服务（ClientDetailsService），客户端详情信息在这里进行初始化，你能够把客户端详情信息写死在这里或者是通过数据库来存储调取详情信息。
+     *
+     * @param clients
+     * @throws Exception
+     */
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
-        endpoints.tokenStore(tokenStore());
-
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        defaultTokenServices.setSupportRefreshToken(false);
-        defaultTokenServices.setClientDetailsService(endpoints.getClientDetailsService());
-        defaultTokenServices.setTokenEnhancer(endpoints.getTokenEnhancer());
-        defaultTokenServices.setAccessTokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30));
-        endpoints.tokenServices(defaultTokenServices);
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("clientapp")
+                .secret("112233")
+                .redirectUris("http://localhost:9001/callback")
+                // 授权码模式
+                .authorizedGrantTypes("authorization_code")
+                .scopes("read_userinfo", "read_contacts");
     }
 }
